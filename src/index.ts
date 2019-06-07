e054a61c4acb143ff27ae5d1dd739ae37a02c88d
Thomas Ruiz <contact@thomasruiz.eu>
2019-06-07 11:52:24 +0200

    added missing exported functions

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@ export { init } from './init';
 export * from './validation';
 
 export { handle, DefaultHandler } from './handling';
+export { before, after, handleError } from './handling/middleware';
+
 export * from './handling/api/types';
